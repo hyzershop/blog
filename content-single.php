d29a2c68562d09b37749528ceba237edcee7c4a5
hyzershop <danny@hyzershop.com>
2015-10-26 21:42:33 -0400

    oops

--- content-single.php
+++ content-single.php
@@ -27,17 +27,18 @@
 
 	<div class="entry-content">
 		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'gazette' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'gazette' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+		?>
 	</div><!-- .entry-content -->
-	<?php
-		wp_link_pages( array(
-			'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'gazette' ) . '</span>',
-			'after'       => '</div>',
-			'link_before' => '<span>',
-			'link_after'  => '</span>',
-			'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'gazette' ) . ' </span>%',
-			'separator'   => '<span class="screen-reader-text">, </span>',
-		) );
-	?>
+	
 	<footer class="entry-footer">
 		<?php gazette_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
