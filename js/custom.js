6f2d6e499e85c673f9691a94b5817e0a103cbec1
hyzershop <danny@hyzershop.com>
2015-10-28 02:42:06 -0400

    sigh

--- js/custom.js
+++ js/custom.js
@@ -96,7 +96,7 @@ $(document).ready(function() {
 			$(".search-form .search-field").focus();
 		}
 	});
-	$(".search-form[aria-expanded='true']").on("blur",".search-field",function() {
+	$(document).on("blur",".search-form[aria-expanded='false'] .search-field",function() {
 		$(".search-toggle").click();
 		console.log("yay");
 	});
