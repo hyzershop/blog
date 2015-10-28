3f16d7d4613b472f74304e2d3de036b4e4e7bab9
hyzershop <danny@hyzershop.com>
2015-10-28 02:44:58 -0400

    hm?

--- js/custom.js
+++ js/custom.js
@@ -96,7 +96,7 @@ $(document).ready(function() {
 			$(".search-form .search-field").focus();
 		}
 	});
-	$(document).on("blur",".search-form[aria-expanded='true'] .search-field",function() {
+	$(document).on("blur",".search-form[aria-expanded='false'] .search-field",function() {
 		$(".search-toggle").click();
 		console.log("yay");
 	});
