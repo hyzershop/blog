ad08d188652c1a484503beed8776b650b3a3e5a3
hyzershop <danny@hyzershop.com>
2015-10-28 19:30:19 -0400

    nav

--- js/custom.js
+++ js/custom.js
@@ -119,7 +119,7 @@ function fullScreenMenu() {
 	if ($(window).width() < 840) {
 		$mainMenu.appendTo("body");
 	} else {
-		$mainMenu.appendTo("#site-navigation");
+		$mainMenu.show().appendTo("#site-navigation");
 	}
 
 	$(".menu-toggle").click(function() {
