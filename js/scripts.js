16f590d6285c786ac8aee46cf77845652eb587cb
hyzershop <danny@hyzershop.com>
2015-10-31 22:00:58 -0400

    safads

--- js/scripts.js
+++ js/scripts.js
@@ -258,7 +258,7 @@ $(document).ready(function() {
 	.siblings("textarea#comment").attr("placeholder", "Write a comment. It's cool, we won't bite.");
 
 	$(".post-category a").each(function() {
-		if ( $(this).attr("href").indexOf("home-page-featured") ) {
+		if ( $(this).attr("href").indexOf("home-page-featured") > -1 ) {
 			$(this).hide();
 		}
 	});
