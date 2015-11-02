3df36a130c9a658e65ac0561e2c0a847f7c5e232
hyzershop <danny@hyzershop.com>
2015-11-01 19:18:36 -0500

    afds

--- js/scripts.js
+++ js/scripts.js
@@ -221,9 +221,10 @@ $(document).ready(function() {
 	/** Category icons in submenu **/
 	$("header > .nav > ul > li > .sub-menu > li > a").each(function() {
 		var strippedURL = $(this).parent().parent().find("a").attr("href").replace("http://blog.hyzershop.com/category/", ""),
-			category = strippedURL.replace("/", "");
-		$("head").append("
-			<style>header > .nav > ul > li > .sub-menu > li > a { background-image: url('/wp-content/themes/hyzer-blog/images/" + category + "-icon-white.png'); }</style>
+			category = strippedURL.replace("/", ""),
+			id = $(this).parent("li").attr("id");
+		$("").append("
+			<style>header > .nav > ul > li > .sub-menu > li#" + id + " > a::before { background-image: url('/wp-content/themes/hyzer-blog/images/" + category + "-icon-white.png'); }</style>
 		");
 	});
 
