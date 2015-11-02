551f2e2c16a89f7b9b2f2de307c61aa2124416c4
hyzershop <danny@hyzershop.com>
2015-11-01 23:34:16 -0500

    dasfdfs

--- js/scripts.js
+++ js/scripts.js
@@ -296,7 +296,7 @@ $(document).ready(function() {
 			}
 		});
 		signupTopExit.click(function() {
-			mc_email_only.hide().parent().addClass("hide-always");
+			mc_email_only.hide().parents(".mc_embed_signup").addClass("hide-always");
 		});
 	});
 
