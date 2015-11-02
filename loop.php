55669331c6320ca6b80b87371d73253636b85256
hyzershop <danny@hyzershop.com>
2015-11-02 01:14:10 -0500

    sadfsfasdfsadf

--- loop.php
+++ loop.php
@@ -28,7 +28,7 @@
 <!-- 		<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
  -->		<!-- /post details -->
 
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+		<div class="excerpt"><?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?></div>
 
 		<?php edit_post_link(); ?>
 
