eef74dc7d4b16ac11ed122e5348e38d3f3622cee
hyzershop <danny@hyzershop.com>
2015-11-02 00:24:21 -0500

    asdffdsa

--- header.php
+++ header.php
@@ -5,9 +5,9 @@
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-		
+        <link href="<?php echo get_template_directory_uri(); ?>/favicon.png" rel="shortcut icon">
+<!--         <link href="<?php // echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+ -->		
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 
