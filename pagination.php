20bd5460deb75bb3976ad546daa0566e188c06ca
hyzershop <danny@hyzershop.com>
2015-11-01 20:07:24 -0500

    add pagination.php

---
+++ pagination.php
@@ -0,0 +1,6 @@
+<!-- pagination -->
+<div class="pagination">
+	<div class="nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
+	<div class="nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
+</div>
+<!-- /pagination -->
